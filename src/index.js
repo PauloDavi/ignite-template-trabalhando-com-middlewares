cc19dc44a43c3c22d597b862d399f00cfa6ada33
Paulo Davi <paulo.araujo@cear.ufpb.br>
2021-11-30 07:13:13 -0300

    finish challenger

--- src/index.js
+++ src/index.js
@@ -10,19 +10,82 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(o => o.username === username);
+
+  if(!user) {
+    return response.status(404).json({
+      error: "Error message"
+    })
+  }
+
+  request.user = user;
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(user.pro) {
+    return next()
+  }
+
+  if(user.todos.length < 10) {
+    return next()
+  }
+
+  return response.status(403).json({
+      error: "Error message"
+    })
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const uuidIsValid = validate(id)
+
+  if(!uuidIsValid) {
+    return response.status(400).json({
+      error: "Error message"
+    })
+  }
+
+  const user = users.find(o => o.username === username);
+
+  if(!user) {
+    return response.status(404).json({
+      error: "Error message"
+    })
+  }
+  
+  const todo = user.todos.find(o => o.id === id)
+
+  if(!todo) {
+    return response.status(404).json({
+      error: "Error message"
+    })
+  }
+
+  request.user = user;
+  request.todo = todo;
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(o => o.id === id);
+
+  if(!user) {
+    return response.status(404).json({
+      error: "Error message"
+    })
+  }
+
+  request.user = user;
+  return next()
 }
 
 app.post('/users', (request, response) => {
